8906e44952d7c17d8e7034448b3af557b73ff978
Scott Cotton <scott@mindowl.com>
2022-06-11 23:31:16 +0200

    dynamic level_test

--- level_test.go
+++ level_test.go
@@ -42,16 +42,25 @@ func tagLevel(l L.Logger, cfg *L.Config, o *L.Obj) *L.Obj {
 	return o.Field("Lop", LogLevel(v).String())
 }
 
+func filterLevel(l L.Logger, cfg *L.Config, o *L.Obj) *L.Obj {
+	v := cfg.Labels["Lop"]
+	if v == 0 {
+		return o
+	}
+	if LogLevel(v) <= Level {
+		return o
+	}
+	return nil
+}
+
 func LConfig(lvl LogLevel) *L.Config {
 	res := &L.Config{
 		Labels: map[string]int{"Lop": int(lvl)},
 		W:      lvlW,
 		F:      L.JSONFmter(),
 		E:      L.EPanic,
-		Pre: []L.Middleware{
-			L.Leq("Lop", int(lvl)),
-		},
-		Post: []L.Middleware{L.Pkg(), tagLevel},
+		Pre:    []L.Middleware{filterLevel},
+		Post:   []L.Middleware{L.Pkg(), tagLevel},
 	}
 	return res
 }
@@ -71,9 +80,16 @@ var (
 func Example_levels() {
 	Ltrace.Dict().Field("hello-trace", 22).Log()
 	Ldebug.Dict().Field("hello-debug", 33).Log()
+
+	Level = Trace
+
+	Ltrace.Dict().Field("hello-trace", 44).Log()
+	Ldebug.Dict().Field("hello-debug", 55).Log()
 	fmt.Printf("%s\n", lvlW.String())
 
 	// Output:
 	// {"hello-debug":33,"Lpkg":"github.com/scott-cotton/L_test","Lop":"debug"}
+	// {"hello-trace":44,"Lpkg":"github.com/scott-cotton/L_test","Lop":"trace"}
+	// {"hello-debug":55,"Lpkg":"github.com/scott-cotton/L_test","Lop":"debug"}
 
 }
