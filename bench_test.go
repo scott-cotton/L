f3bc5a8142be06f683db0e43a4c34a70c4e94611
Scott Cotton <scott@mindowl.com>
2022-06-14 10:31:34 +0200

    add some benchmarks

--- bench_test.go
+++ bench_test.go
@@ -7,13 +7,36 @@ import (
 	"github.com/scott-cotton/L"
 )
 
-func BenchmarkBasic(b *testing.B) {
+func BenchmarkConstruct1(b *testing.B) {
+	b.StopTimer()
 	L := L.New(&L.Config{
 		Labels: map[string]int{},
 		W:      io.Discard,
-		//F:      &L.CLI{Fields: []string{"key3", "key0"}},
-		F: L.JSONFmter(),
-		E: L.EPanic,
+		F:      L.JSONFmter(),
+		E:      L.EPanic,
+	})
+	o := L.Dict().
+		Field("key0", 22).
+		Field("key2", false).
+		Field("key3", "hello susan").
+		Field("key4", "bjez").
+		Field("key5", 22.0).
+		Field("key6", 77e10).
+		Field("key7", "seven").
+		Field("key8", "nine")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		o.Clone().Field("nine", "nine")
+	}
+}
+
+func BenchmarkBasic(b *testing.B) {
+	b.StopTimer()
+	L := L.New(&L.Config{
+		Labels: map[string]int{},
+		W:      io.Discard,
+		F:      L.JSONFmter(),
+		E:      L.EPanic,
 	})
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
@@ -22,5 +45,22 @@ func BenchmarkBasic(b *testing.B) {
 			Field("key2", false).
 			Field("key3", "hello susan").
 			Log()
+	}
+}
+
+func BenchmarkConstructDict(b *testing.B) {
+	b.StopTimer()
+	L := L.New(&L.Config{
+		Labels: map[string]int{},
+		W:      io.Discard,
+		F:      L.JSONFmter(),
+		E:      L.EPanic,
+	})
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		L.Dict().
+			Field("key0", 22).
+			Field("key2", false).
+			Field("key3", "hello susan")
 	}
 }
